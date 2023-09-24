a7a498ba916b50ec24249aa82abeb2be77781d62
罗乐 <13420106+luo-qiren@user.noreply.gitee.com>
2023-09-24 21:08:22 +0800

    feat: "ser"

--- epidemic-backend/src/main/java/peris/decadez/epidemicbackend/entity/User.java
+++ epidemic-backend/src/main/java/peris/decadez/epidemicbackend/entity/User.java
@@ -1,26 +1,39 @@
 package peris.decadez.epidemicbackend.entity;
 
-import com.baomidou.mybatisplus.annotation.EnumValue;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import peris.decadez.epidemicbackend.entity.Enum.GenderEnum;
+
+import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Data
-public class User {
+public class User implements Serializable {
+
   @TableId(type = IdType.AUTO)
+
   private Long id;
+
   private String name;
+
   private String username;
+
   private Integer age;
+
   private GenderEnum sex;
+
   private String email;
+
   private String idCard;
+
   private String phone;
+
   private String address;
+
   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private String password;
+
+  @TableField(fill = FieldFill.INSERT)
   private Timestamp createAt;
 }
