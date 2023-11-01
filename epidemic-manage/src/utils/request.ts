fe246c4d6906b4dd96e433afba373f55896aa352
decadez <decadez@yeah.net>
2023-11-01 22:32:41 +0800

    update: 封装request，提取css功能函数等；

--- epidemic-manage/src/utils/request.ts
+++ epidemic-manage/src/utils/request.ts
@@ -1,32 +1,65 @@
-import umiRequest from 'umi-request';
+import Qs from 'query-string'
+import { extend } from 'umi-request'
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'http://localhost:8080'
 
-const request = (
-  url: string,
-  {
-    method = 'GET',
-    params,
-    noAccess = false,
-  }: { method: string; params?: any; noAccess?: boolean }
-) => {
-  let accessToken = null;
-  if (!noAccess) {
-    accessToken = localStorage.getItem('persist:root')
-      ? JSON.parse(localStorage.getItem('persist:root')).token
-      : null;
+enum StatusCode {
+  SUCCESS = 200,
+  ERROR = 500,
+  NOT_FOUND = 404,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  TIMEOUT = 408,
+}
+// 与后端约定的响应数据格式
+interface ResponseStructure {
+  success: boolean
+  data: any
+  code?: StatusCode
+  errorMessage?: string
+}
 
-    accessToken = accessToken.substr(1);
-    accessToken = accessToken.substring(0, accessToken.length - 1);
-  }
+const accessToken = JSON.parse(localStorage.getItem('persist:root'))?.token;
 
-  return umiRequest(`${baseUrl}${url}`, {
-    method,
-    [method === 'GET' ? 'params' : 'data']: params,
-    headers: {
-      token: accessToken,
-    },
-  });
-};
+const request = extend({
+  prefix: baseUrl,
+  headers: {
+    "Content-Type": "application/json",
+    token: accessToken.substring(1, accessToken.length - 1),
+  },
+  paramsSerializer(params) {
+    return Qs.stringify(params, { arrayFormat: 'bracket' })
+  },
+  // 错误处理
+  errorHandler: (error: any) => {
+    const { response } = error
+    if (response) {
+      const { status, statusText, data } = response
+      const { success, code, errorMessage } =
+        data as ResponseStructure
+      if (success) {
+        return
+      }
+      switch (code) {
+        case StatusCode.ERROR:
+          return
+        case StatusCode.NOT_FOUND:
+          console.warn(errorMessage)
+          return
+        case StatusCode.UNAUTHORIZED:
+          console.error(errorMessage)
+          return
+        case StatusCode.FORBIDDEN:
+          return
+        case StatusCode.TIMEOUT:
+          return
+        default:
+          return
+      }
+    } else {
+      console.error(error)
+    }
+  },
+})
 
 export default request;
